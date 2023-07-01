46c5a91142c2dc57a2cc08a55eeef8e4934fe7ac
Noor Dawod <hi@noordawod.me>
2023-07-01 16:59:30 +0200

    Add a new base class for entities with a `String` primary key.

--- src/main/kotlin/org/noordawod/kotlin/orm/entity/StringKeyEntity.kt
+++ src/main/kotlin/org/noordawod/kotlin/orm/entity/StringKeyEntity.kt
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2022 Noor Dawod. All rights reserved.
+ * Copyright 2023 Noor Dawod. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -21,40 +21,19 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-@file:Suppress("unused", "MemberVisibilityCanBePrivate")
+@file:Suppress("unused")
 
 package org.noordawod.kotlin.orm.entity
 
-import org.noordawod.kotlin.core.extension.publicIdOrEmpty
-import org.noordawod.kotlin.core.repository.HashValue
-import org.noordawod.kotlin.core.security.ByteArrayStrength
-import org.noordawod.kotlin.core.security.ByteUtils
-
 /**
- * Generic top-level class for all entities having a key with a [HashValue] value type.
+ * Generic top-level class for all entities having a key with a [String] value type.
  */
-abstract class HashValueKeyEntity protected constructor() : BaseKeyEntity<HashValue>() {
-  override fun toString(): String = id.publicIdOrEmpty()
+abstract class StringKeyEntity protected constructor() : BaseKeyEntity<String>() {
+  override fun toString(): String = id
 
   override fun equals(other: Any?): Boolean =
-    other is HashValueKeyEntity && id.contentEquals(other.id)
+    other is StringKeyEntity && id == other.id
 
   @Suppress("RedundantOverride")
   override fun hashCode(): Int = super.hashCode()
-
-  companion object {
-    /**
-     * Generates a random [HashValue] with a length corresponding with the provided [strength].
-     *
-     * @param strength determines the length of the [HashValue]
-     */
-    fun randomId(strength: ByteArrayStrength): HashValue = randomId(strength.length)
-
-    /**
-     * Generates a random [HashValue] with length equal to specified value.
-     *
-     * @param length length of [HashValue]
-     */
-    fun randomId(length: Int): HashValue = ByteUtils.randomBytes(length)
-  }
 }
